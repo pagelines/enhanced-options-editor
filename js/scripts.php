66e9546e97c26e5bbabdaff2620033f379374f29
William Mincy <william.mincy@jwt.com>
2013-08-20 08:26:31 -0400

    + Added support for textareas + Fixed call stack JS recursio error

--- js/scripts.php
+++ js/scripts.php
@@ -2,61 +2,75 @@
 <script>
 		jQuery(document).ready(function() {
 			console.log('DMS Options');
+			var workflag = false;
 			
-			jQuery('div.panel-section-options[data-key="section-options"] #local .tab-panel-inner').on('DOMSubtreeModified', function(event) {
-					//console.log('Added : ');
+			jQuery('div.panel-section-options[data-key="section-options"] .tab-panel-inner').on('DOMSubtreeModified', function(event) {
+					if(workflag) { return; }
+					workflag = true;
+					
 					$targ = jQuery(event.target);
 					if($targ.attr('class') == 'panel-tab-content')
 					{
-						$inputs = $targ.find('input');
+						$inputs = $targ.find('input[type="text"],textarea');
 						if($inputs.length > 0)
 						{
 							$inputs.each(
 									function() {
 											$this = jQuery(this);
-											if( $this.attr('type')!="text" ) {
-												return;
-											}
-											$this.css({width:'80%'});
-											$this.wrap('<div />').parent().append('<input type="button" value="&#164;" class="wmHtmlOverlayEditorButton" style="width:20px;height:24px;margin:0 0 10px 10px;font-size:18px;" />');
-											$btn = $this.parent().find('.wmHtmlOverlayEditorButton');
-											$btn.button().click(
-													function() {
-															if(!jQuery('.bootbox').hasClass('in')) {
-																bootbox.alert('<div id="wmHtmlOverlayEditorTextareaWrapper" style="width:100%;height:400px;"><textarea id="wmHtmlOverlayEditorTextarea" style="height:360px;">'+$this.val()+'</textarea><input type="hidden" name="wmHtmlOverlayEditorTarget" id="wmHtmlOverlayEditorTarget" value="'+$this.attr('name')+'" /></div>', 
-																	function() {
-																		$content = tinymce.activeEditor.getContent();
-																		$target = jQuery('#wmHtmlOverlayEditorTarget').val();
-																		
-																		console.log($content);
-																		console.log(jQuery('#wmHtmlOverlayEditorTextarea').val());
-																		
-																		if($content!='') { jQuery('input[name="'+$target+'"]').val($content); }
-																	});
+											$this.css({width:'82%'});
+											$this.wrap('<div />').parent().append('<a class="btn" href="#" style="margin:0 0 10px 10px;"><i class="icon-star wmHtmlOverlayEditorButton"></i></a>');
+											$btn = $this.next();
+											if($btn.length!=0) {
+												$btn.button().click(
+														function(event) {
+																event.preventDefault();
+																$this = jQuery(this).parent().find('input,textarea');
+																if(!jQuery('.bootbox').hasClass('in')) {
+																	bootbox.alert('<div id="wmHtmlOverlayEditorTextareaWrapper" style="width:100%;height:400px;"><textarea id="wmHtmlOverlayEditorTextarea" style="height:360px;">'+$this.val()+'</textarea><input type="hidden" name="wmHtmlOverlayEditorTarget" id="wmHtmlOverlayEditorTarget" value="'+$this.attr('name')+'" /></div>', 
+																		function() {
+																			$content = tinymce.activeEditor.getContent();
+																			$target = jQuery('#wmHtmlOverlayEditorTarget').val();
+																			$control = jQuery('input[name="'+$target+'"],textarea[name="'+$target+'"]');
+																			if($content!='') { $control.val($content); }
+																			tinymce.remove();
+																		});
+																	tinymce.init({
+																			selector: "#wmHtmlOverlayEditorTextarea",
+																			relative_urls : true,
+																			menubar:false,
+																			statusbar: false,
+																			document_base_url:  "<?php echo site_url(); ?>/wp-content/plugins/wm-dms-htmleditor/js/tinymce/",
+																			plugins: [
+																				"advlist autolink lists link image charmap print preview anchor",
+																				"searchreplace visualblocks code fullscreen",
+																				"insertdatetime media table contextmenu paste"
+																			],
+																			toolbar: "undo redo | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent "
+																		});
 																	
-																tinymce.init({
-																		selector: "#wmHtmlOverlayEditorTextarea",
-																		relative_urls : true,
-																		menubar:false,
-																		statusbar: false,
-																		document_base_url:  "<?php echo site_url(); ?>/wp-content/plugins/wm-dms-htmleditor/js/tinymce/",
-																		plugins: [
-																			"advlist autolink lists link image charmap print preview anchor",
-																			"searchreplace visualblocks code fullscreen",
-																			"insertdatetime media table contextmenu paste"
-																		],
-																		toolbar: "undo redo | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent "
-																	});
-															} else {
-																window.bootbox.hideAll();
+																	tinymce.activeEditor.setContent($this.val());
+																} else {
+																	$content = tinymce.activeEditor.getContent();
+																	$target = jQuery('#wmHtmlOverlayEditorTarget').val();
+																	if($content!='') { jQuery('input[name="'+$target+'"]').val($content); }
+																	tinymce.remove();
+																	window.bootbox.hideAll();
+																}
 															}
-														}
-												);
+													);
+												}//endif
 										}
 								);
 						}
 					}
+					
+					workflag = false;
 				});
 				
 		});
 </script>
+<style>
+	.modal { z-index:10500; width: 600px; }
+	.modal-backdrop { z-index:10400; }
+	.modal-body { padding: 0; }
+</style>

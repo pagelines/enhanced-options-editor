38f69c1bd7bbd9f591ef50b41107244a50bbd8dd
William Mincy <william.mincy@jwt.com>
2013-09-01 10:41:34 -0400

    + Replaced hard-coded modal CSS changes with add/remove classes as to not globaly alter the modals for DMS + Shored up auto-saving so that content will not be lost on page reloads

--- js/scripts.php
+++ js/scripts.php
@@ -35,13 +35,16 @@
 																			$control = jQuery('input[name="'+$target+'"],textarea[name="'+$target+'"]');
 																			if($content!='') { 
 																				$control.val('');
+																				$control.focus().blur();
+																				$control.val($content);
 																				setTimeout(function () {
-																					$control.focus().blur();
-																					$control.val($content);
 																					setTimeout(function() { $control.focus().blur(); }, 200); 
+																					jQuery('.modal-backdrop').removeClass('eoe-modal-backdrop');
+																					jQuery('.modal').removeClass('eoe-modal');
 																				}, 200);
 																			}
 																			tinymce.remove();
+																			
 																			jQuery('div.bootbox div.modal-footer #saveWithoutFormatting').off('click').remove();
 																		});
 																	tinymce.init({
@@ -62,8 +65,12 @@
 																			tabfocus_elements: ":prev,:next"
 																		});
 																	tinymce.activeEditor.setContent($this.val());
+																	
+																	jQuery('.modal').addClass('eoe-modal');
+																	jQuery('.modal-backdrop').addClass('eoe-modal-backdrop');
+																	
 																	var $bootbox = jQuery('div.bootbox');
-																	$bootbox.css({'width':'80%', 'left': '25%', 'margin-left': '-15%'});
+																	
 																	var $bootboxFooter = jQuery('div.modal-footer', $bootbox);
 																	$bootboxFooter.find('a').html('Save With Formatting');
 																	$bootboxFooter.prepend('<a href="#" id="saveWithoutFormatting" class="btn btn-default">Save Without Formatting</a>');
@@ -116,20 +123,34 @@
 																				return str.replace(/^\s+|\s+$/g, '').replace(/\n\s*\n/g, '\n');
 																			}
 																			$control.val('');
+																			$control.focus().blur();
+																			$control.val(strip_tags($content)); 
 																			setTimeout(function() {
-																				$control.focus().blur();
-																				$control.val(strip_tags($content)); 
 																				setTimeout(function() { $control.focus().blur(); }, 200);
+																				jQuery('.modal-backdrop').removeClass('eoe-modal-backdrop');
+																				jQuery('.modal').removeClass('eoe-modal');
 																			},200);
 																		}
+																		
+																		
 																		tinymce.remove();
 																		jQuery('div.bootbox div.modal-footer #saveWithoutFormatting').off('click').remove();
 																		window.bootbox.hideAll();
 																	});
 																} else {
 																	$content = tinymce.activeEditor.getContent();
+																	$control = jQuery('input[name="'+$target+'"],textarea[name="'+$target+'"]');
 																	$target = jQuery('#wmHtmlOverlayEditorTarget').val();
-																	if($content!='') { jQuery('input[name="'+$target+'"]').val($content); }
+																	if($content!='') { 
+																		$control.val('');
+																		$control.focus().blur();
+																		$control.val(strip_tags($content)); 
+																		setTimeout(function() {
+																			setTimeout(function() { $control.focus().blur(); }, 200);																			
+																			jQuery('.modal-backdrop').removeClass('eoe-modal-backdrop');
+																			jQuery('.modal').removeClass('eoe-modal');
+																		},200);
+																	}
 																	tinymce.remove();
 																	window.bootbox.hideAll();
 																}
@@ -145,7 +166,7 @@
 		});
 </script>
 <style>
-	.modal { z-index:10500; width: 600px; }
-	.modal-backdrop { z-index:10400; }
+	.eoe-modal { z-index:10500; width: 80%; left: 25%; margin-left: -15%; }
+	.eoe-modal-backdrop { z-index:10400; }
 	.modal-body { padding: 0; }
 </style>
